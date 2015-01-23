a8332f6956901411a423a2b1c6eb90dfe7794f4f
Matthew Riley MacPherson <matt@lonelyvegan.com>
2015-01-22 22:14:57 -0500

    Tweak index.js style

--- index.js
+++ index.js
@@ -4,17 +4,13 @@
 /* global -Promise */
 var Promise = require('es6-Promise').Promise;
 
-module.exports = findApp;
-
 function findApp(options) {
-  
   options = options || {};
 
   var client = options.client;
   var manifest = options.manifest;
 
   return new Promise(function(resolve, reject) {
-
     if (client === undefined || manifest === undefined) {
       return reject(new Error('We need a client and a manifest to find an app'));
     }
@@ -25,47 +21,37 @@ function findApp(options) {
       .then(function(result) {
         resolve(result);
       });
-
   });
-
 }
 
 
 function getWebAppsActor(client) {
   return new Promise(function(resolve, reject) {
-
     client.getWebapps(function(err, webapps) {
       if (err) {
         return reject(err);
       }
 
       resolve(webapps);
-
     });
-
   });
 }
 
 
 function listInstalledApps(webAppsActor) {
   return new Promise(function(resolve, reject) {
-
     webAppsActor.getInstalledApps(function(err, apps) {
-
       if (err) {
         return reject(err);
       }
 
       resolve(apps);
-
     });
-
   });
 }
 
 
 function filterAppsByName(name) {
-
   return function(apps) {
     return new Promise(function(resolve) {
       resolve(apps.filter(function(app) {
@@ -73,5 +59,6 @@ function filterAppsByName(name) {
       }));
     });
   };
-
 }
+
+module.exports = findApp;
